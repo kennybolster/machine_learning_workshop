b8722588b8c94e65a7249ed5e7d1a5279dfd34a0
Kenny Bolster <54642100+kennybolster@users.noreply.github.com>
2022-11-21 13:07:10 -0600

    doing first rf model

--- predict_ba.R
+++ predict_ba.R
@@ -6,20 +6,33 @@ woa = read_csv('woa_2_deg.csv')
 
 print(idp)
 
-ggplot(idp) + geom_point(mapping = aes(longitude, latitude))
+ggplot(idp) + geom_point(mapping = aes(longitude, latitude)) + 
+  ggtitle('Stations in full IDP')
 plot(last_plot())
 
 n_na = apply(idp, 2, function(x) sum(is.na(x)))
 print(n_na)
 
-idp <- idp %>% drop_na()
-ggplot(idp) + geom_point(mapping = aes(longitude, latitude))
+allvar_idp <- idp %>% drop_na()
+ggplot(allvar_idp) + geom_point(mapping = aes(longitude, latitude)) +
+  ggtitle('Stations with all variables measured')
 plot(last_plot())
 
 set.seed(1551)
-trainind = sample(1:nrow(idp), .7*nrow(idp))
-train_idp = idp[trainind,]
-test_idp = idp[-trainind,]
+trainind = sample(1:nrow(allvar_idp), .7*nrow(allvar_idp))
+train_idp = allvar_idp[trainind,]
+test_idp = allvar_idp[-trainind,]
 
+allvar_rf = randomForest(barium ~ temp + sal + oxy + phosphate + nitrate + silicate,
+                         data = train_idp)
+train_idp$pred = predict(allvar_rf, train_idp)
+ggplot(train_idp) + geom_point(aes(barium, pred)) + ggtitle('All variables - training')
+plot(last_plot())
+print(cor(train_idp$barium, train_idp$pred))
 
+test_idp$pred = predict(allvar_rf, test_idp)
+p = ggplot(test_idp) + geom_point(aes(barium, pred)) +
+  ggtitle('All variables - test')
+plot(p)
+print(cor(test_idp$barium, test_idp$pred))
 
